043f42f07e49d26ddc6ff98fc61c01edb83da094
G_Gurneet <the.gurneet@gmail.com>
2019-09-15 21:15:19 +0100

    update

---
+++ DynamicWithNode/app.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Setting the views engine
+app.set('view engine', 'ejs');
+app.set('views', 'DynamicWithNode/views')
+//set the route for the views folder
+
+
+
+//Loading the Routes
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Using the Routes
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+
+app.use((req, res, next) => {
+    res.status(404).render('404' , {pageTitle: 'Page Not Found'});
+});
+
+app.listen(3000, ()=>{
+    console.log("Server started on Localhost:3000")
+});
--- cms/app.js
+++ cms/app.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const app = express(); 
-<<<<<<< HEAD
 const path = require('path');
 const expressHandlebars = require('express-handlebars');
 
+
+
+
 app.use(express.static(path.join(__dirname + '/public')))
+
+//set View Engine
 app.engine('handlebars',expressHandlebars({defaultLayout : 'home'}))    //template engine   looks inside views->layout by default. 
 app.set('view engine','handlebars');
 
+// set View Path
 var view = path.join(__dirname +  '/views')
 app.set('views', view)
 
-app.get('/',(req,res)=>{
-    res.render('home/index')
-})
 
-app.get('/about',(req,res)=>{
-    res.render('home/about')
-})
+//Load the Routes
+const home = require('./routes/home/index');
+const admin = require('./routes/admin/index');
+//Use the Routes
+app.use('/', home);
+app.use('/admin', admin)
 
 
+
+// Start the Server
 app.listen(4500, ()=>{
-    console.log("Server started on 4500");
-    console.log(__dirname)
+    console.log("Server started on localhost:4500");
 })
 
 
 
-=======
-
-app.listen(4500, ()=>{
-    console.log('Server started on 4500')
-})
->>>>>>> 890e9d8a602eacae2cbb02987bc66812751c7a73
